c7e5dbef5e3a24155b05d549982bf3f23224ad20
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-09 12:14:01 -0500

    :construction: implement whitelist

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -30,6 +30,7 @@ export class ProductsService {
   }
 
   create(payload: CreateProductDto) {
+    console.log(payload);
     this.counterId = this.counterId + 1;
     const newProduct = {
       id: this.counterId,
