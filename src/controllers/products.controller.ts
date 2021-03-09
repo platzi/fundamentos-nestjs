17b1c28dd5af994ead8b6cba75d53068a8747f92
Nicolas Molina Monroy <hi@nicobytes.com>
2021-03-09 09:41:08 -0500

    :construction: work in status codes

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -7,7 +7,12 @@ import {
   Body,
   Put,
   Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
 } from '@nestjs/common';
+
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
@@ -28,8 +33,11 @@ export class ProductsController {
   }
 
   @Get(':productId')
-  getOne(@Param('productId') productId: string) {
-    return `product ${productId}`;
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Res() response: Response, @Param('productId') productId: string) {
+    response.status(200).send({
+      message: `product ${productId}`,
+    });
   }
 
   @Post()
